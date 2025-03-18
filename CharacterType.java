c83c9e2c3d1fc04a4eb955b591a3e93401725ef2
fazlerabbi13 <mohammad0132794@gmail.com>
2025-03-18 17:25:25 +0600

    Determine character type (vowel, consonant, digit, special)

---
+++ CharacterType.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CharacterType {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char ch = scanner.next().charAt(0);
+        if ("AEIOUaeiou0".indexOf(ch) != -1)
+            System.out.println("Vowel");
+        else if (Character.isDigit(ch))
+            System.out.println("Digit");
+        else if ((ch >= 'B' && ch <= 'Z') || (ch >= 'b' && ch <= 'z'))
+            System.out.println("Consonant");
+        else
+            System.out.println("Special Character");
+        scanner.close();
+    }
+}
+
