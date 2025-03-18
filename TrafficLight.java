a49a7cb00e274c98893a540a678f8da6346daf63
fazlerabbi13 <mohammad0132794@gmail.com>
2025-03-18 17:13:55 +0600

    Traffic Light Simulation

---
+++ TrafficLight.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TrafficLight {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char signal = scanner.next().charAt(0);
+        
+        if(signal == 'R')
+        System.out.println("Stop");
+            
+            else if(signal=='Y')
+             System.out.println("Ready");
+              
+            else if(signal=='G')
+            System.out.println("Go"); 
+
+            else
+            System.out.println("Invalid input");
+
+            
+        }
+        
+    }
+
